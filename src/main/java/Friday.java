7a3a3d13f1f1ea1e28075f19495371706de1df08
jvally <135511441+jvally@users.noreply.github.com>
2026-08-20 12:30:11 +0800

    Add a echo feature and personality to chatbot for user inputs

--- src/main/java/Friday.java
+++ src/main/java/Friday.java
@@ -1,13 +1,51 @@
+import java.util.Scanner;
+
 public class Friday {
-    //used Codex 5.4 mini to help me fill in template code for the message
+    //used ChatGPT 5.4-mini to help me do the ascii art
+    private static final String GOOD_DAY_ART = """
+            ________________________________
+           |                                |
+           |  Good day to you sir!          |
+           |________________________________|
+            """;
+
+    private static final String THANKS_ART = """
+            ________________________
+           |                        |
+           |  Thanks!               |
+           |________________________|
+            """;
+
     public static void main(String[] args) {
         String separator = "____________________________________________________________";
+        Scanner scanner = new Scanner(System.in);
 
         System.out.println(separator);
-        System.out.println("What is up! I'm Friday.");
-        System.out.println("How can i be of assistance today");
+        System.out.println("Wassup! I'm Friday, your personalized academic weapon.");
+        System.out.println("What do you need today?");
         System.out.println(separator);
-        System.out.println("See ya soon!");
-        System.out.println(separator);
+
+        while (scanner.hasNextLine()) {
+            String command = scanner.nextLine();
+
+            System.out.println(separator);
+            if (command.equals("bye")) {
+                System.out.println("Bye. I hope I DON'T see you again soon:)");
+                System.out.println(separator);
+                break;
+            } else if (command.equals("hello")) {
+                System.out.println(GOOD_DAY_ART);
+                continue;
+            } else if (command.equals("thanks")) {
+                System.out.println(THANKS_ART);
+                continue;
+            } else if (command.equals("help")) {
+                System.out.println("Sure. Here you go:");
+                System.out.println("https://nus-cs2103-ay2627-s1.github.io/website/schedule/week2/project.html");
+                continue;
+            }
+
+            System.out.println(command);
+        }
     }
 }
